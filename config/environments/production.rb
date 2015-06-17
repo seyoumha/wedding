c8f9945c70b8e540eeadbeb277867730a32affbe
Hanna Seyoum <seyoumha@gmail.com>
2015-06-17 14:47:09 -0400

    updated scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,7 @@ Wedding::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
+  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
@@ -77,4 +76,7 @@ Wedding::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.app_domain = 'jawiandsamra.com'
 end
