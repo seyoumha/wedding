e43c1fedf3ac2dd89db0dfbda1372d2e18fe8c46
Hanna <hanna@Toto.local>
2016-02-29 15:37:48 -0500

    added .mp4 to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Wedding::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-   config.assets.precompile += %w(jquery.fullPage.min.js jquery.fullPage.css )
+   config.assets.precompile += %w(jquery.fullPage.min.js jquery.fullPage.css  *.mp4 )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
